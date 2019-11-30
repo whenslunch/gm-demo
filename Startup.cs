2d2cc56fbadd623ad472454d8c7bcc7e4de06706
Tze Lin Ong <tzong@microsoft.com>
2019-11-30 09:38:26 -0800

    change to hello azure

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace gm_demo
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello Azure!");
                 });
             });
         }
