8d86f406e3c95ee74e6d16943cfe466082500409
Tze Lin Ong <tzong@microsoft.com>
2019-11-30 09:41:47 -0800

    minor changes

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace gm_demo
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello Azure!");
+                    await context.Response.WriteAsync("Hello Azure!!!");
                 });
             });
         }
